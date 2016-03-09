74a47e5fb4dc168515d9c6c85be8ec09f388776c
antomio <toms.pirktins@gmail.com>
2016-03-09 21:13:34 +0200

    array with 100 variables

--- code.js
+++ code.js
@@ -1,9 +1,25 @@
 (function($) {
 
-	
+	var array = [];
 	for(var i = 0; i < 100; i++) {
-    	console.log(i);
-    }
+		array[array.length] = i;
+	}
+ console.log(array);
 	
 
 })(jQuery);
+
+// var skolens = {
+//   tel: "+37122334455",
+//   grades: [7,6,3,4],
+//   average: function() {
+//   	var sum = 0;
+    
+//     for(var i = 0; i < this.grades.length; i++) {
+//       sum += this.grades[i];
+//     }
+    
+//     return sum / this.grades.length;
+//   }
+// }
+
