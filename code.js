763c9f4a861a4d1367ddfba669fa20f6131fc2ae
Tomsantomio <toms.pirktins@gmail.com>
2016-03-10 16:53:49 +0200

    majasdarbs2
    -2validacijas lauki
    -1. ievadam array elementu skaitu
    -2. ievadam pirmo elementu

--- code.js
+++ code.js
@@ -1,15 +1,49 @@
-(function($) {
+// (function($) {
 
-	var array = [];
-	for(var i = 0; i < 100; i++) {
-		array[array.length] = Math.floor((Math.random() * 100) + 1);
+// 	var array = [];
+// 	for(var i = 0; i < 100; i++) {
+// 		array[array.length] = Math.floor((Math.random() * 100) + 1);
 
 	
-	}
+// 	}
 
-console.log(array);
+// console.log(array);
 
-})(jQuery);
+// })(jQuery);
+
+
+
+// $( document ).ready(function() {
+// 	$( "#target" ).click(function(e) {
+// 		e.preventDefault();
+// 		validation($( "input[name='name']" ).val(), $( "input[name='surname']" ).val());
+// 	});
+// });
+
+(function($) {
+	$("#target").on("click", function(e) {
+		e.preventDefault();
+		validation($( "input[name='name']" ).val(), $( "input[name='surname']" ).val());
+  });
+})(jQuery);	
+
+	function validation(value, value2) {
+		var array = [];
+		for(var i = 0; i < value; i++){
+			array[array.length] = Math.floor((Math.random() * 100) + 1);
+		}
+		if (!value || !value2) {
+			alert("Tabulā ir 0 elementu!");
+		}
+		else {
+			array[0] = value2;
+		}
+	 	console.log(array);
+	};
+
+
+		
+	
 
 // var skolens = {
 //   tel: "+37122334455",
